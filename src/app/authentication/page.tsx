88ec8f7c8730060984021cc4bce3c2bc9e1fba3f
felipekgouvea <felipekinupesg@hotmail.com>
2025-08-28 15:51:29 -0300

    chore: ajustes

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -11,22 +11,22 @@ const AuthenticationPage = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
-  // if (session?.user) {
-  //   redirect("/dashboard");
-  // }
+  if (session?.user) {
+    redirect("/dashboard");
+  }
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <Tabs defaultValue="login" className="w-[400px]">
-        {/* <TabsList className="grid w-full grid-cols-2">
+        <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="login">Login</TabsTrigger>
           <TabsTrigger value="register">Criar conta</TabsTrigger>
-        </TabsList> */}
+        </TabsList>
         <TabsContent value="login">
           <LoginForm />
         </TabsContent>
-        {/* <TabsContent value="register">
+        <TabsContent value="register">
           <SignUpForm />
-        </TabsContent> */}
+        </TabsContent>
       </Tabs>
     </div>
   );
