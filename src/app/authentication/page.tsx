0116fd5481e0ff966d45e8a8e095253d28acbdec
felipekgouvea <felipekinupesg@hotmail.com>
2025-08-28 15:54:12 -0300

    chore: ajuste

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,32 +1,32 @@
 import { headers } from "next/headers";
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { auth } from "@/lib/auth";
 
-import SignUpForm from "./components/sign-up-form";
+// import SignUpForm from "./components/sign-up-form";
 import LoginForm from "./components/login-form";
 
 const AuthenticationPage = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
-  if (session?.user) {
-    redirect("/dashboard");
-  }
+  // if (session?.user) {
+  //   redirect("/dashboard");
+  // }
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <Tabs defaultValue="login" className="w-[400px]">
-        <TabsList className="grid w-full grid-cols-2">
+        {/* <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="login">Login</TabsTrigger>
           <TabsTrigger value="register">Criar conta</TabsTrigger>
-        </TabsList>
+        </TabsList> */}
         <TabsContent value="login">
           <LoginForm />
         </TabsContent>
-        <TabsContent value="register">
+        {/* <TabsContent value="register">
           <SignUpForm />
-        </TabsContent>
+        </TabsContent> */}
       </Tabs>
     </div>
   );
